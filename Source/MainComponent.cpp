e417503b09e609fe2afda73712efea41bd08ff6b
parednuw <mail@paulwunder.de>
2023-06-17 22:36:43 +0200

    Added basic easy changeable number of omits

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -60,7 +60,6 @@ MainComponent::MainComponent()
 	mCountdownChooser.addItem("20", 4);
 	mCountdownChooser.addItem("25", 5);
 	mCountdownChooser.addItem("30", 6);
-	mCountdownChooser.addItem("1", 7);
 	mCountdownChooser.setJustificationType(juce::Justification::centred);
 	mCountdownChooser.setSelectedId(1);
 	mCountdownChooser.onChange = [this]() { chooseCountdown(); };
@@ -142,11 +141,13 @@ void MainComponent::omitClick()
 	{
 		omitStatus = true;
 		mMetronome.setOmitCount(0);
+		mOmitButton.setButtonText("Omitting");
 		mOmitButton.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colour(0xFF003068));
 	}
 	else if (omitStatus == true)
 	{
 		omitStatus = false;
+		mOmitButton.setButtonText("Omit");
 		mOmitButton.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colour(0xFF573068));
 		
 	}
@@ -172,8 +173,6 @@ void MainComponent::chooseCountdown()
 		mCountdown.setCountdown(25);
 	if (id == 6)
 		mCountdown.setCountdown(30);
-	if (id == 7)
-		mCountdown.setCountdown(1);
 }
 
 void MainComponent::startCountdown()
@@ -221,12 +220,15 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
 
 	float level = juce::Decibels::decibelsToGain(mMetronome.getSliderLevel());
 	
-	if (omitStatus && mMetronome.getOmitCount() > 4)
+	int numberOfClicksToOmit = 5;
+	
+	if (omitStatus && mMetronome.getOmitCount() > numberOfClicksToOmit)
 	{
 		level = 0;
-		if (mMetronome.getOmitCount() == 9)
+		if (mMetronome.getOmitCount() == (numberOfClicksToOmit * 2 + 1))
 			mMetronome.setOmitCount(1);
 	}
+	
 	bufferToFill.buffer->applyGain(bufferToFill.startSample, bufferToFill.numSamples, level);
 	mMetronome.setTempo(MainComponent::mTempoSlider.getValue());
 }
